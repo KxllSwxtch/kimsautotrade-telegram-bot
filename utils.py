fa9b3772ac72d6c7f762c2f7849995cb6bc68284
Dmitriy Shin <dimashin9@gmail.com>
2025-05-24 14:51:37 +0900

    fixed proxies issue

--- utils.py
+++ utils.py
@@ -4,11 +4,8 @@ import datetime
 import locale
 from kgs_customs_table import KGS_CUSTOMS_TABLE
 
-HTTP_PROXY = 'http://B01vby:GBno0x@45.118.250.2:8000'
-proxies = {
-    'http': HTTP_PROXY,
-    'https': HTTP_PROXY
-}
+HTTP_PROXY = "http://B01vby:GBno0x@45.118.250.2:8000"
+proxies = {"http": HTTP_PROXY, "https": HTTP_PROXY}
 
 
 # Очищение памяти
@@ -53,7 +50,7 @@ def get_customs_fees_russia(
     }
 
     try:
-        response = requests.post(url, data=payload, headers=headers, proxies=proxies)
+        response = requests.post(url, data=payload, headers=headers)
         response.raise_for_status()
         return response.json()
     except requests.RequestException as e:
